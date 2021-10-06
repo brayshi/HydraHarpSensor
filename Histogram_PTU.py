acf12a4a1d6020355a7f4546075ed6174361ac7d
Brayden Shinkawa <braydenshinkawa@gmail.com>
2021-10-06 14:28:44 -0700

    Histogram and Trace combined in Trace_PTU

--- Histogram_PTU.py
+++ Histogram_PTU.py
@@ -13,13 +13,24 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
+import time
 import sys
 import struct
-import os
 from functools import partial
-import time
-import matplotlib.widgets as widget
+import os
 from queue import Queue
+# static variables
+# channel numbers
+GREEN = 2
+RED = 1
+
+
+# number of overflows needed before plotting on graph
+# calculation being done is 75 ns * 1023 (number of bits in nsync) * OVERFLOW_MAX
+# this comes out to around 0.1 s per overflow, or 100 ms when OVERFLOW_MAX is 1300
+OVERFLOW_MAX = 1300
+MAX_HEIGHT = 1e5
+THOUSAND_MILLISECONDS = 1000
 
 # Tag Types
 tyEmpty8      = struct.unpack(">i", bytes.fromhex("FFFF0008"))[0]
@@ -122,94 +133,38 @@ tagValues = [tagDataList[i][1] for i in range(0, len(tagDataList))]
 globRes = tagValues[tagNames.index("MeasDesc_GlobalResolution")] # the period of the histogram
 measDescRes = tagValues[tagNames.index("MeasDesc_Resolution")] # the resolution of the measurements being done for each dtime
 
-# if the command doesn't contain both Tail_PTU.py and the PTU file, the command will exit without an output
-if len(sys.argv) != 2:
-    print("USAGE: Tail_PTU.py newFile.ptu")
-    exit(0)
-
-inputfile = open(sys.argv[1], "rb")
-
-# if the PTU file isn't a PicoQuant PTU file, exit and print error
-magic = inputfile.read(8).decode("utf-8").strip('\0')
-if magic != "PQTTTR":
-    print("ERROR: Magic invalid, this is not a PTU file.")
-    inputfile.close()
-    exit(0)
-
-inputfile.seek(0, os.SEEK_END) #End-of-file. Next read will get to EOF.
-
-# channel numbers
-GREEN = 2
-RED = 1
-
-# number of data points allowed on the graph
-TRACE_SIZE = 100
-# number of overflows needed before plotting on graph
-# calculation being done is 75 ns * 1023 (number of bits in nsync) * OVERFLOW_MAX
-# this comes out to around 0.1 s per overflow, or 100 ms when OVERFLOW_MAX is 1300
-OVERFLOW_SECOND = 13000
-TRACE_OVERFLOW = OVERFLOW_SECOND / 1000
-HIST_OVERFLOW = OVERFLOW_SECOND / 10
-TRACE_HEIGHT = 50
-HIST_HEIGHT = 1e5
-THOUSAND_MILLISECONDS = 1000
-
-# number of data points allowed on the graph
-BIN_INPUT = 64 * 1e-12
-FOUR_PICO_MULTIPLE = int(BIN_INPUT / measDescRes)
-binSize = measDescRes*FOUR_PICO_MULTIPLE # 4 ps * 16 = 64 ps bins
-NUM_BINS = int(-(globRes // -binSize)) # "ceiling" division for number of bins needed
-
 # global variables
 global ofl
 global green_lst
 global red_lst
-global green_bins
-global red_bins
 
 # initialize subplots for the graph
-fig, (trace_ax, hist_ax) = plt.subplots(1, 2)
+fig, ax = plt.subplots()
 
-# unbind default key bindings
-fig.canvas.mpl_disconnect(fig.canvas.manager.key_press_handler_id)
+# number of data points allowed on the graph
+binSize = measDescRes*16 # 4 ps * 16 = 64 ps bins
+NUM_BINS = int(-(globRes // -binSize)) # "ceiling" division for number of bins needed
+
+inputfile.seek(0, os.SEEK_END) #End-of-file. Next read will get to EOF.
 
 # initialize global variables
 ofl = 0
-idx = 0
-x = np.arange(0, TRACE_SIZE*np.float16(TRACE_OVERFLOW/13000), np.float16(TRACE_OVERFLOW/13000))
-green_lst = np.ones(TRACE_SIZE, dtype=np.uint32, order='C')
-green_trace, = trace_ax.plot(x, green_lst, 'g-')
-red_lst = np.ones(TRACE_SIZE, dtype=np.uint32, order='C')
-red_trace, = trace_ax.plot(x, red_lst, 'r-')
-
 HIST_BINS = np.linspace(0, globRes*1e9, num=NUM_BINS, endpoint=True)
-green_bins = np.ones(NUM_BINS, dtype=np.uint32, order='C')
-green_hist, = hist_ax.plot(HIST_BINS, green_bins, 'g-')
-red_bins = np.ones(NUM_BINS, dtype=np.uint32, order='C')
-red_hist, = hist_ax.plot(HIST_BINS, red_bins, 'r-')
-
+green_lst = np.ones(NUM_BINS, dtype=np.uint32, order='C')
+green_line, = ax.plot(HIST_BINS, green_lst, 'g-')
+red_lst = np.ones(NUM_BINS, dtype=np.uint32, order='C')
+red_line, = ax.plot(HIST_BINS, red_lst, 'r-')
 buffer = Queue()
 
-def changeTraceHeight(value):
-    TRACE_HEIGHT = int(value)
-    trace_ax.set_ylim([1, TRACE_HEIGHT])
-
 # initializes the figure, axis, and passes artists through
-def init_fig(fig, trace_ax, hist_ax, artists):
-    # set up trace's values
-    trace_ax.set_title('Time Trace Live Plot')
-    trace_ax.set_xlabel('Time [s]')
-    trace_ax.set_ylabel('Counts per {amount} ms bin'.format(amount=int(TRACE_OVERFLOW/13)))
-    trace_ax.set_xlim([0, TRACE_SIZE*np.float16(TRACE_OVERFLOW/13000)])
-
-    # set up hist's values
-    hist_ax.set_title('HIstogram Live Plot')
-    hist_ax.set_xlabel('Time [ns]')
-    hist_ax.set_ylabel('Counts per {size} ps bin'.format(size = int(binSize*1e12)))
-    hist_ax.grid(True)
-    hist_ax.semilogy()
-    hist_ax.set_ylim([10, HIST_HEIGHT])
-    hist_ax.set_xlim([0, globRes*1e9])
+def init_fig(fig, ax, artists):
+    # set up plot's values
+    plt.title('Histogram Live Plot')
+    plt.xlabel('Time [ns]')
+    plt.ylabel('Counts per 64 ps bin')
+    plt.grid(True)
+    plt.yscale('log')
+    ax.set_ylim([10, MAX_HEIGHT])
 
     return artists
 
@@ -224,13 +179,10 @@ def frame_iter():
         buffer.put("{0:0{1}b}".format(struct.unpack("<I", recordData)[0], 32))
     
     yield buffer
-
-# Used to animate the graph based off of what has been saved into the buffer
-def animate(buffer, red_trace, green_trace, red_hist, green_hist):
-    global ofl, green_lst, red_lst, green_bins, red_bins
-
+# Used to animate the graph based off of what is being written into the PTU file
+def animate(buffer, red_line, green_line):
+    global ofl, green_lst, red_lst
     while not buffer.empty():
-        
         recordData = buffer.get()
         special = int(recordData[0:1], base=2)
         channel = int(recordData[1:7], base=2)
@@ -244,42 +196,23 @@ def animate(buffer, red_trace, green_trace, red_hist, green_hist):
                     ofl += 1
                 else:
                     ofl += nsync
-            
-            if ofl >= TRACE_OVERFLOW * TRACE_SIZE: # once the overflow amount is over a threshold,
-                # add the values into the graph's lists
-
-                # draw new trace frame
-                red_trace.set_data(x, red_lst)
-                green_trace.set_data(x, green_lst)
-                # draw new histogram frame
-                red_hist.set_data(HIST_BINS, red_bins)
-                green_hist.set_data(HIST_BINS, green_bins)
+            if ofl >= OVERFLOW_MAX: # once the overflow amount is over a threshold,
+                # draw new graph frame (OVERFLOW_MAX's value causes the graph to return every 100 ms)
+                red_line.set_data(HIST_BINS, red_lst)
+                green_line.set_data(HIST_BINS, green_lst)
                 # reset the values, and finish the function call
                 ofl = 0
-                green_lst = [0] * TRACE_SIZE
-                red_lst = [0] * TRACE_SIZE
                 break
-        else: # regular input channel to count 100 ms bins
-            trace_indx = int(ofl // TRACE_OVERFLOW)
-            hist_indx = int((dtime * measDescRes)//binSize)-1 # this bins out where the dtime corresponds to and which bin it should go into (by dividing it into binSize)
+        else: # regular input channel to count bins
+            indx = int((dtime * measDescRes)//binSize)-1 # this bins out where the dtime corresponds to and which bin it should go into (by dividing it into binSize)
             if int(channel) == GREEN:
-                green_bins[hist_indx] += 1
-                green_lst[trace_indx] += 1
+                green_lst[indx] += 1
             elif int(channel) == RED:
-                red_bins[hist_indx] += 1
-                red_lst[trace_indx] += 1
-        
-    return red_trace, green_trace, red_hist, green_hist
+                red_lst[indx] += 1
+    return red_line, green_line
 
-update = partial(animate, red_trace=red_trace, green_trace=green_trace, red_hist=red_hist, green_hist=green_hist)
-init = partial(init_fig, fig=fig, trace_ax=trace_ax, hist_ax=hist_ax, artists=(red_trace,green_trace))
-
-axbox = fig.add_axes([0.1, 0.05, 0.4, 0.05])
-text_box = widget.TextBox(axbox, "Trace Height")
-text_box.on_submit(changeTraceHeight)
-text_box.set_val(100)
-
-
+update = partial(animate, red_line=red_line, green_line=green_line)
+init = partial(init_fig, fig=fig, ax=ax, artists=(red_line, green_line))
 
 # FuncAnimation calls animate for the figure that was passed into it at every interval
 ani = animation.FuncAnimation(fig=fig, func=update, frames=frame_iter, init_func = init, interval=THOUSAND_MILLISECONDS/10000.0, blit=True)
